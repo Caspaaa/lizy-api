c54397bef3983bf840318d2563338e4ad8e09341
Caspaaa <e.baudelle@gmail.com>
2021-05-11 18:01:52 +0200

    added pre hook to UserSchema to hash passwords with bcrypt before saving

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,8 +1,32 @@
+import { ErrorRequestHandler, NextFunction } from "express";
 require("./dbconnect");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
 
 const UserSchema = new mongoose.Schema({
   login: { type: String, required: true },
   password: { type: String, required: true },
+});
+
+UserSchema.pre("save", function (this: any, next: NextFunction) {
+  if (this.isNew || this.isModified("password")) {
+    const document = this;
+    bcrypt.hash(
+      document.password,
+      saltRounds,
+      (error: ErrorRequestHandler, hashedPassword: string) => {
+        if (error) {
+          next(error);
+        } else {
+          document.password = hashedPassword;
+          next();
+        }
+      }
+    );
+  } else {
+    next();
+  }
 });
 
 module.exports = mongoose.model("User", UserSchema);
